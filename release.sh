36d4a1ac556a34e98f3dbf77cd5d13d4f8531337
CodeGoy <144829693+CodeGoy@users.noreply.github.com>
2026-05-09 02:45:02 -0400

    script update

--- release.sh
+++ release.sh
@@ -3,14 +3,13 @@
 wsEndpoint="ws"
 addr="10.1.10.194"
 port="8443"
-names=("chad" "stacy" "john" "jane" "scott")
-passwords=("password1234!" "password1234!" "password1234!" "password1234!" "password1234!")
+users=("chad:password1234!" "stacy:password1234!" "john:password1234!" "jane:password1234!" "scott:password1234!")
 serverKey="Yd7OH1x6v53HSlantzjQFdWyx5GogR5v"
 cert="./certs/selfsigned.crt"
 key="./certs/selfsigned.key"
 
 # script vars
-num_names=${#names[@]}
+num_users=${#users[@]}
 programName="ogsma"
 clients=""
 
@@ -22,31 +21,38 @@ go mod tidy && go build .
 cd ../
 
 # Generate keystore files for names
-for (( i=0; i<num_names; i++ )); do
-  echo "generating keystore file for: ${names[$i]} with pass: ${passwords[$i]}"
-  ./keystore_gen/keystore_gen  --password "${passwords[$i]}" --new "${names[$i]}" --keystore "${names[$i]}.keystore"
+for (( i=0; i<num_users; i++ )); do
+    IFS=':' read -ra USER <<< "${users[$i]}"
+    name="${USER[0]}"
+    pass="${USER[1]}"
+    echo "generating keystore file for: ${name} with pass: ${pass}"
+    ./keystore_gen/keystore_gen  --password "${pass}" --new "${name}" --keystore "${name}.keystore"
 done
 
 # Add contacts to keystore
-for (( i=0; i<num_names; i++ )); do
-  targetName="${names[$i]}"
-  echo "adding contacts to keystore for: ${targetName}"
-  for (( j=0; j<num_names; j++ )); do
-    contactName="${names[$j]}"
-    if [[ "$targetName" != "$contactName" ]]; then
-      echo "adding: ${contactName} to keystore for: ${targetName}"
-      ./keystore_gen/keystore_gen --password "${passwords[$i]}" --add "${contactName}.keyshare" --keystore "${targetName}.keystore"
-    fi
-  done
+for (( i=0; i<num_users; i++ )); do
+    IFS=':' read -ra USER <<< "${users[$i]}"
+    pass="${USER[1]}"
+    name="${USER[0]}"
+    echo "adding contacts to keystore for: ${name}"
+    for (( j=0; j<num_users; j++ )); do
+        IFS=':' read -ra SUBUSER <<< "${users[$j]}"
+        contactName="${SUBUSER[0]}"
+        if [[ "$name" != "$contactName" ]]; then
+            echo "adding: ${contactName} to keystore for: ${name}"
+            ./keystore_gen/keystore_gen --password "${pass}" --add "${contactName}.keyshare" --keystore "${name}.keystore"
+        fi
+    done
 done
 
 # generate client config.json
-for (( i=0; i<num_names; i++ )); do
-  clients+="${names[$i]},"
-  targetName="${names[$i]}"
-  keystoreString=$(cat "${targetName}.keystore")
-  echo "generating config.json file for: ${targetName}"
-  ./config_gen/config_gen --type client --keystore "${keystoreString}" --port "${port}" --addr "${addr}" --endpoint "${wsEndpoint}" --sk "${serverKey}" --opf "${targetName}_config.json"
+for (( i=0; i<num_users; i++ )); do
+    IFS=':' read -ra USER <<< "${users[$i]}"
+    name="${USER[0]}"
+    clients+="${name},"
+    keystoreString=$(cat "${name}.keystore")
+    echo "generating config.json file for: ${name}"
+    ./config_gen/config_gen --type client --keystore "${keystoreString}" --port "${port}" --addr "${addr}" --endpoint "${wsEndpoint}" --sk "${serverKey}" --opf "${name}_config.json"
 done
 
 # generate server config file
@@ -68,13 +74,14 @@ cd ../
 # build client for names
 cd ./client || exit
 go mod tidy
-for (( i=0; i<num_names; i++ )); do
-    targetName="${names[$i]}"
-    echo "building executable for ${targetName}"
-    cp "../${targetName}_config.json" ./config.json
-    go build -o "../${targetName}_${programName}" .
+for (( i=0; i<num_users; i++ )); do
+    IFS=':' read -ra USER <<< "${users[$i]}"
+    name="${USER[0]}"
+    echo "building executable for ${name}"
+    cp "../${name}_config.json" ./config.json
+    go build -o "../${name}_${programName}" .
     ANDROID_NDK_HOME="$HOME/Android/android-ndk-r21e" fyne p --release --os android/arm64
-    mv ./ogsma.apk "../${targetName}_${programName}.apk"
+    mv ./ogsma.apk "../${name}_${programName}.apk"
     rm config.json
 done
 cd ../
